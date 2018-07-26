231a2247e79c4075c074b436d2919a3769de95cd
Fiona Rizzo <frizzo@rowlandschools.org>
2018-07-26 07:59:21 -0700

    Add Rizzo

--- team9.py
+++ team9.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team 9' # Only 10 chars displayed.
+strategy_name = 'Betray until We win'
+strategy_description = 'We decided to betray at the beginning or at any point of the time when we have less or the same score as the opponent'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if my_score == their_score or my_score < their_score:
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
